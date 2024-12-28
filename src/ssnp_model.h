b8ac56175213d976c5edd4649ede01e59e34b79e
Andrew Xin <andrewx@bu.edu>
2024-12-27 16:38:53 -0800

    tilt converted up to xr calculation

--- src/ssnp_model.h
+++ src/ssnp_model.h
@@ -5,14 +5,26 @@
 #include <complex>
 
 std::vector<float> scatter_factor(const std::vector<float>& n, float res_z = 0.1, float dz = 1.0, float n0 = 1.0);
+
 std::vector<std::vector<std::complex<float>>> c_gamma(const std::vector<float>& res, const std::vector<int>& shape);
-std::pair<std::vector<std::vector<std::vector<std::complex<double>>>>, 
-          std::vector<std::vector<std::vector<std::complex<double>>>>>
-diffract(const std::vector<std::vector<std::complex<double>>>& uf,
-         const std::vector<std::vector<std::complex<double>>>& ub,
-         const std::vector<float>& res = {0.1f, 0.1f, 0.1f},
-         float dz = 1);
+
+std::pair<std::vector<std::vector<std::vector<std::complex<double>>>>, std::vector<std::vector<std::vector<std::complex<double>>>>>
+diffract(
+    const std::vector<std::vector<std::complex<double>>>& uf,
+    const std::vector<std::vector<std::complex<double>>>& ub,
+    const std::vector<float>& res = {0.1f, 0.1f, 0.1f},
+    float dz = 1);
+
 std::vector<std::vector<bool>> binary_pupil(
-    const std::vector<int>& shape, float na, const std::vector<float>& res = {0.1f, 0.1f, 0.1f});
+    const std::vector<int>& shape, 
+    float na, 
+    const std::vector<float>& res = {0.1f, 0.1f, 0.1f});
+
+std::vector<std::vector<std::vector<std::complex<double>>>> tilt(
+    const std::vector<int>& shape,
+    const std::vector<double>& angles,
+    double NA = 0.65,
+    const std::vector<double>& res = {0.1f, 0.1f, 0.1f},
+    bool trunc = true);
 
 #endif
