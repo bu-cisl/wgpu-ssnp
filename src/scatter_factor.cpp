0548b67489461f802a6379004df6d12f4785dd9f
Andrew Xin <andrewx@bu.edu>
2025-03-18 15:58:50 -0400

    starting abstraction

--- src/scatter_factor.cpp
+++ src/scatter_factor.cpp
@@ -5,6 +5,7 @@
 #define WEBGPU_CPP_IMPLEMENTATION
 #include <webgpu/webgpu.hpp>
 #include "scatter_factor.h"
+#include "webgpu_utils.h"
 
 // INPUT PARAMS
 struct Params {
@@ -14,44 +15,6 @@ struct Params {
 };
 
 size_t buffer_len;
-
-// INITIALIZING WEBGPU
-bool init_wgpu(wgpu::Instance& instance, wgpu::Adapter& adapter, wgpu::Device& device, wgpu::Queue& queue) {
-    // Create an instance
-    wgpu::InstanceDescriptor instanceDescriptor = {};
-    instance = wgpu::createInstance(instanceDescriptor);
-    if (!instance) {
-        std::cerr << "Failed to create WebGPU instance." << std::endl;
-        return false;
-    }
-
-    // Request adapter
-    wgpu::RequestAdapterOptions adapterOptions = {};
-    adapterOptions.powerPreference = wgpu::PowerPreference::HighPerformance;
-    adapter = instance.requestAdapter(adapterOptions);
-    if (!adapter) {
-        std::cerr << "Failed to request a WebGPU adapter." << std::endl;
-        return false;
-    }
-
-    // Request device
-    wgpu::DeviceDescriptor deviceDescriptor = {};
-    deviceDescriptor.label = "Default Device";
-    device = adapter.requestDevice(deviceDescriptor);
-    if (!device) {
-        std::cerr << "Failed to request a WebGPU device." << std::endl;
-        return false;
-    }
-
-    // Retrieve command queue
-    queue = device.getQueue();
-    if (!queue) {
-        std::cerr << "Failed to retrieve command queue." << std::endl;
-        return false;
-    }
-
-    return true;
-}
 
 // LOADING AND COMPILING SHADER CODE
 std::string readShaderFile(const std::string& filename) {
