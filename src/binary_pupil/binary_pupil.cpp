a2ebee0de276b169187e05cb30b849124b151995
Andrew Xin <andrewx@bu.edu>
2025-03-29 00:19:02 -0400

    creating bind group and layout

--- src/binary_pupil/binary_pupil.cpp
+++ src/binary_pupil/binary_pupil.cpp
@@ -1,2 +1,64 @@
 #include "binary_pupil.h"
 
+struct Params {
+    float na;
+};
+
+static size_t buffer_len;
+
+// CREATING BIND GROUP AND LAYOUT
+static wgpu::BindGroupLayout createBindGroupLayout(wgpu::Device& device) {
+    wgpu::BindGroupLayoutEntry cgammaBufferLayout = {};
+    cgammaBufferLayout.binding = 0;
+    cgammaBufferLayout.visibility = wgpu::ShaderStage::Compute;
+    cgammaBufferLayout.buffer.type = wgpu::BufferBindingType::ReadOnlyStorage;
+
+    wgpu::BindGroupLayoutEntry maskBufferLayout = {};
+    maskBufferLayout.binding = 1;
+    maskBufferLayout.visibility = wgpu::ShaderStage::Compute;
+    maskBufferLayout.buffer.type = wgpu::BufferBindingType::Storage;
+
+    wgpu::BindGroupLayoutEntry uniformBufferLayout = {};
+    uniformBufferLayout.binding = 2;
+    uniformBufferLayout.visibility = wgpu::ShaderStage::Compute;
+    uniformBufferLayout.buffer.type = wgpu::BufferBindingType::Uniform;
+
+    wgpu::BindGroupLayoutEntry entries[] = {cgammaBufferLayout, maskBufferLayout, uniformBufferLayout};
+
+    wgpu::BindGroupLayoutDescriptor layoutDesc = {};
+    layoutDesc.entryCount = 3;
+    layoutDesc.entries = entries;
+
+    return device.createBindGroupLayout(layoutDesc);
+}
+
+static wgpu::BindGroup createBindGroup(wgpu::Device& device, wgpu::BindGroupLayout bindGroupLayout, wgpu::Buffer cgammaBuffer, wgpu::Buffer maskBuffer, wgpu::Buffer uniformBuffer) {
+    wgpu::BindGroupEntry cgammaEntry = {};
+    cgammaEntry.binding = 0;
+    cgammaEntry.buffer = cgammaBuffer;
+    cgammaEntry.offset = 0;
+    cgammaEntry.size = sizeof(float) * buffer_len;
+
+    wgpu::BindGroupEntry maskEntry = {};
+    maskEntry.binding = 1;
+    maskEntry.buffer = maskBuffer;
+    maskEntry.offset = 0;
+    maskEntry.size = sizeof(uint32_t) * buffer_len;
+
+    wgpu::BindGroupEntry uniformEntry = {};
+    uniformEntry.binding = 2;
+    uniformEntry.buffer = uniformBuffer;
+    uniformEntry.offset = 0;
+    uniformEntry.size = sizeof(Params);
+
+    wgpu::BindGroupEntry entries[] = {cgammaEntry, maskEntry, uniformEntry};
+
+    wgpu::BindGroupDescriptor bindGroupDesc = {};
+    bindGroupDesc.layout = bindGroupLayout;
+    bindGroupDesc.entryCount = 3;
+    bindGroupDesc.entries = entries;
+
+    return device.createBindGroup(bindGroupDesc);
+}
+
+void binary_pupil(WebGPUContext& context, wgpu::Buffer& maskBuffer, std::vector<float> cgamma, float na);
