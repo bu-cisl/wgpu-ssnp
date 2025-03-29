4b93b5d5f692aac7ee49f3257f211f5b98eb8604
Andrew Xin <andrewx@bu.edu>
2025-03-28 23:57:37 -0400

    adding files for binary_pupil

---
+++ src/binary_pupil/binary_pupil.h
@@ -0,0 +1,15 @@
+#ifndef BINARY_PUPIL_H
+#define BINARY_PUPIL_H
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <vector>
+#include <optional>
+#include <cmath>
+#include <webgpu/webgpu.hpp>
+#include "../webgpu_utils.h"
+#include "../c_gamma/c_gamma.h"
+
+
+
+#endif 
