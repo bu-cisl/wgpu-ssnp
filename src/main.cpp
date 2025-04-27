49c56d504918eb134fb9bb685a39c2791ba0574f
Rayan Syed <rsyed@bu.edu>
2025-04-27 00:52:41 -0400

    starting input field config

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,10 @@
 using namespace std;
 
 int main() {
+    // Initialize WebGPU
+    WebGPUContext context;
+    initWebGPU(context);
+
     // default init params for ssnp model
     vector<float> res = {0.1,0.1,0.1};
     float na = 0.65;
@@ -25,7 +29,7 @@ int main() {
     bool intensity = true;
 
     // angles_size vectors of c_ba values
-    vector<vector<double>> angles(angles_size, vector<double>(2, 0.0));
+    vector<vector<float>> angles(angles_size, vector<float>(2, 0.0));
 
     // input matrix
     vector<vector<vector<float>>> n(3, vector<vector<float>>(16, vector<float>(16, 1.0f)));
@@ -33,7 +37,16 @@ int main() {
     // ssnp forward function
     vector<int> shape = {int(n[0].size()), int(n[0][0].size())};
     
-    for(vector<double> c_ba : angles) {
+    for(vector<float> c_ba : angles) {
+        // CONFIGURING INPUT FIELD
+
+        // Generate Forward/Backward
+        wgpu::Buffer tiltResultBuffer = createBuffer(context.device, nullptr, sizeof(float) * shape[0] * shape[1] * 2, WGPUBufferUsage(wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc));
+        tilt(context, tiltResultBuffer, c_ba, shape, res);
+        vector<float> backward(shape[0]*shape[1]*2, 0.0);
+        wgpu::Buffer backwardBuffer = createBuffer(context.device, backward.data(), sizeof(float) * shape[0] * shape[1] * 2, WGPUBufferUsage(wgpu::BufferUsage::Storage | wgpu::BufferUsage::CopySrc));
+        backwardBuffer = backwardBuffer;
+
         continue;
     }
 
