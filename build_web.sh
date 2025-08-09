10f5727524bcbf9f613c1ea5149ed1ee6c80f3cc
Rayan Syed <rsyed@bu.edu>
2025-08-09 04:02:06 -0400

    new build setup

--- build_web.sh
+++ build_web.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# Create docs/ dir for build
 module load python3
 source ./emsdk/emsdk_env.sh
-emcmake cmake -B docs
 
-# Build + save html as index for git
+emcmake cmake -B docs
 cmake --build docs
-cp ssnp_cpp.html docs/index.html
-cp UTIF.js docs/UTIF.js
+
 python -m http.server -d docs
