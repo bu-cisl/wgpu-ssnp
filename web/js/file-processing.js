670c582aada3ca353ae6f6604c3515f06d0c8b5b
Rayan Syed <rsyed@bu.edu>
2025-08-09 21:06:58 -0400

    refactor tiff decoding logic

--- web/js/file-processing.js
+++ web/js/file-processing.js
@@ -74,3 +74,74 @@ function convertTiffDataToFloat32(raw, metadata) {
 	
 	return output;
 }
+
+function decodeTiffToRaw(arrayBuffer) {
+	const ifds = UTIF.decode(arrayBuffer);
+	
+	if (ifds.length === 0) {
+		throw new Error("No image slices found in TIFF");
+	}
+
+	UTIF.decodeImage(arrayBuffer, ifds[0]);
+	const firstIfd = ifds[0];
+	
+	const width = firstIfd.width || firstIfd['t256']?.[0];
+	const height = firstIfd.height || firstIfd['t257']?.[0];
+	const bps = firstIfd.bps || firstIfd['t258']?.[0] || 8;
+	const spp = firstIfd.spp || firstIfd['t277']?.[0] || 1;
+	const sampleFormat = firstIfd.sampleFormat || firstIfd['t339']?.[0] || 1;
+	const photometric = firstIfd.photometric || firstIfd['t262']?.[0];
+	
+	if (!width || !height) {
+		throw new Error("Could not determine image dimensions");
+	}
+
+	console.log("TIFF Metadata:", {
+		width,
+		height,
+		bitsPerSample: bps,
+		samplesPerPixel: spp,
+		sampleFormat: getSampleFormatName(sampleFormat),
+		photometricInterpretation: getPhotometricName(photometric),
+		compression: firstIfd.compression,
+		planarConfiguration: firstIfd.planarConfig,
+		ifdEntries: Object.keys(firstIfd).filter(k => k.startsWith('t'))
+	});
+
+	const slices = [];
+	for (let i = 0; i < ifds.length; i++) {
+		UTIF.decodeImage(arrayBuffer, ifds[i]);
+		const ifd = ifds[i];
+		const raw = ifd.data;
+		
+		if (!raw) {
+			throw new Error(`Could not decode slice #${i}`);
+		}
+
+		const sliceWidth = ifd.width || ifd['t256']?.[0];
+		const sliceHeight = ifd.height || ifd['t257']?.[0];
+		if (sliceWidth !== width || sliceHeight !== height) {
+			throw new Error(`Slice ${i} dimensions (${sliceWidth}x${sliceHeight}) don't match first slice (${width}x${height})`);
+		}
+
+		const floatSlice = convertTiffDataToFloat32(raw, {
+			bitsPerSample: bps,
+			samplesPerPixel: spp,
+			sampleFormat,
+			width,
+			height
+		});
+
+		if (spp > 1) {
+			const singleChannel = new Float32Array(width * height);
+			for (let p = 0; p < width * height; p++) {
+				singleChannel[p] = floatSlice[p * spp];
+			}
+			slices.push(singleChannel);
+		} else {
+			slices.push(floatSlice);
+		}
+	}
+
+	return { slices, width, height, depth: slices.length };
+}
