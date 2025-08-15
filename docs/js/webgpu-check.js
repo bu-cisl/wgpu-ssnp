f2e93ca26dc5a4c3cdc69475b90bd2c5c6517c29
Rayan Syed <rayansyed@icloud.com>
2025-08-14 21:06:31 -0400

    workflow for build/deploy

--- docs/js/webgpu-check.js
+++
@@ -1,8 +0,0 @@
-// WebGPU compatibility check
-if (!navigator.gpu) {
-	alert("Your browser does not support WebGPU. Please use a compatible browser such as the latest version of Google Chrome or Edge.");
-	document.addEventListener("DOMContentLoaded", () => {
-		document.getElementById("fileInput").disabled = true;
-		document.getElementById("runBtn").disabled = true;
-	});
-}
