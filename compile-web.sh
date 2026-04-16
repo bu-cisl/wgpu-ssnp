9aa8bace65cb98015f60622993d3c0b6f9bd0613
Rayan Syed <rsyed@bu.edu>
2026-04-16 15:31:52 -0400

    expose born approx on frontend + faster web building

--- compile-web.sh
+++ compile-web.sh
@@ -3,6 +3,6 @@ module load python3
 source ./emsdk/emsdk_env.sh
 
 emcmake cmake -B docs
-cmake --build docs
+cmake --build docs -j
 
 python -m http.server -d docs
--- web/js/dom-builder.js
+++ web/js/dom-builder.js
@@ -7,7 +7,7 @@ function createAppStructure() {
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 					<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
 				</svg>
-				Andrew Xin, Rayan Syed
+				Rayan Syed, Andrew Xin
 			</a>
 		</div>
 		<div id="controls">
@@ -66,7 +66,8 @@ function createAppStructure() {
 				<label for="modelType">Model</label>
 				<select id="modelType">
 					<option value="ssnp" selected>SSNP</option>
-					<option value="bpm"> BPM </option>
+					<option value="bpm">BPM</option>
+					<option value="born">Born</option>
 				</select>
 			</div>
 			<button id="runBtn" disabled>Run Simulation</button>
